53aae9c2e3172ff5d59ae2bbf028390bbbdd6caa
streaker03 <andrewgostomelsky@gmail.com>
2020-10-13 22:58:33 -0400

    bat broke

---
+++ Enemy/Bat.gd
@@ -0,0 +1,24 @@
+extends KinematicBody2D
+
+var player = null
+onready var ray = $RayCast2D
+export var speed = 250
+export var looking_speed = 100
+	
+func _physics_process(_delta):
+	#for i in get_slide_count():
+	#	var collision = get_slide_collision(i)
+	#	if collision.collider.name == "Player":
+	#		collision.collider.call("die")
+	if player == null:
+		player = get_node("/root/Game/Player_Container/Player")
+	else:
+		ray.cast_to = ray.to_local(player.global_position)
+		var c = ray.get_collider()
+		if c:
+			var velocity = ray.cast_to.normalized()*looking_speed
+			if c.name == "Player":
+				velocity = ray.cast_to.normalized()*speed
+			print(velocity)
+			move_and_slide(velocity, Vector2(0,0))
+
